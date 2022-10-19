ae072d75834a29993532adc909085fda1d7d5a66
Tomasz Zdeb <tomasz.zdeb@capgemini.com>
2022-10-19 10:26:51 +0200

    Linked product-list stylesheet with component

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -3,7 +3,8 @@ import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: [ './product-list.component.css' ]
 })
 export class ProductListComponent {
   //when default value is provided, there is no
